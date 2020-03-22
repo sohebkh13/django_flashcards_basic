47573e5f2f1cf3a1b932bcc35d84d5e66a2dcb5b
Soheb <sohebkhan10@gmail.com>
2020-03-22 16:43:29 +0530

    initial commit

---
+++ flashcard_website/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FlashcardWebsiteConfig(AppConfig):
+    name = 'flashcard_website'
---
+++ flashes/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for flashes project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flashes.settings')
+
+application = get_wsgi_application()
